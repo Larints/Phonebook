9de2a0b9bc14f0772216f20c80e7c7ed5d12b909
unknown <comp119@comp0119.egadm.ru>
2023-06-23 10:32:20 +0300

    add class and modules

--- Phonebook_module/controller.py
+++ Phonebook_module/controller.py
@@ -1,39 +1,50 @@
+import text
 import view
 import model
-import text
 
 
 def start():
+    pb = model.PhoneBook()
     while True:
         select = view.menu()
         match select:
             case 1:
-                if model.open_file():
-                    view.print_message(text.load_sccessful)
+                if pb.open_file():
+                    view.print_message(text.load_successful)
                 else:
                     view.print_message(text.error_load)
             case 2:
-                if model.save_file():
+                if pb.save_file():
                     view.print_message(text.save_successful)
                 else:
                     view.print_message(text.error_save)
             case 3:
-                view.show_contacts(model.phone_book, text.empty_book)
+                view.show_contacts(pb.get(), text.empty_book)
             case 4:
                 new = view.add_contact()
-                model.add_contact(new)
+                pb.add_contact(new)
                 view.print_message(text.add_successful(new.get('name')))
             case 5:
-                word = view.search_word()
-                result = model.search(word)
+                word = view.view_input(text.search_word)
+                result = pb.search(word)
                 view.show_contacts(result, text.empty_search(word))
-            case 6:
-                word = view.search_word()
-                result = model.search(word)
-                view.show_contacts(result, text.empty_search(word))
-
+            case 6:  # Костыль номер раз
+                view.show_contacts(pb.get(), text.empty_book)
+                act_id = pb.search_id(text.index_change)
+                change_list = view.change(text.contact_change)
+                pb.change_contact(act_id, change_list)
+                view.print_message(text.change_success())
             case 7:
-                pass
+                word = view.view_input(text.search_word)
+                result = pb.search(word)
+                view.show_contacts(result, text.empty_search(word))
+                index = view.view_input(text.index_remove)
+                name = pb.remove(index)
+                view.print_message(text.remove_contact(name))
             case 8:
-                pass
+                if pb.check_on_exit():
+                    answer = view.view_input(text.change_confirm)
+                    if answer != 'n':
+                        pb.save_file()
+                view.print_message(text.goodbay)
                 break
--- Phonebook_module/model.py
+++ Phonebook_module/model.py
@@ -1,47 +1,76 @@
 import json
 
-phone_book = {}
-path = 'phones.json'
+import text
 
 
-def open_file():
-    global phone_book
-    try:
-        with open(path, 'r', encoding='UTF-8') as file:
-            phone_book = json.load(file)
+class PhoneBook:
+
+    def __init__(self, path: str = 'phones.json'):
+        self.contact: dict = {}
+        self.not_changed = {}
+        self.path = path
+
+    def get(self, index: int | None = None):
+        if index:
+            return self.contact.get(index)
+        return self.contact
+
+    def open_file(self):
+        try:
+            with open(self.path, 'r', encoding='UTF-8') as file:
+                self.contact = json.load(file)
+                self.not_changed = self.contact.copy()
+            return True
+        except:
+            return False
+
+    def save_file(self):
+        try:
+            with open(self.path, 'w', encoding='UTF-8') as file:
+                json.dump(self.contact, file, ensure_ascii=False, indent=4)
+            return True
+        except:
+            return False
+
+    def search(self, word: str) -> dict[int:dict[str, str]]:
+        result = {}
+        for index, contact in self.contact.items():
+            if word.lower() in ' '.join(contact.values()).lower():
+                result[index] = contact
+        return result
+
+    def check_id(self):
+        if self.contact:
+            return max(list(map(int, self.contact))) + 1
+        return 1
+
+    def add_contact(self, new: dict[str, str]):
+        contact = {self.check_id(): new}
+        self.contact.update(contact)
+
+    def search_id(self, word: str):  # Костыль номер два
+        while True:
+            id = input(text.index_change)
+            if id in self.contact:
+                return id
+            else:
+                print(text.not_id(id))
+                continue
+
+    def change_contact(self, id: str, contact: []) -> dict[int:dict[str, str]]:
+        if id in self.contact:
+            self.contact[id]['name'] = contact[0]
+            self.contact[id]['phone'] = contact[1]
+            self.contact[id]['comment'] = contact[2]
+            return self.contact
+        else:
+            return text.not_id(id)
+
+    def remove(self, index):
+        name = self.contact.pop(str(index))
+        return name.get('name')
+
+    def check_on_exit(self):
+        if self.contact == self.not_changed:
+            return False
         return True
-    except:
-        return False
-
-
-def save_file():
-    try:
-        with open(path, 'w', encoding='UTF-8') as file:
-            json.dump(phone_book, file, ensure_ascii=False)
-        return True
-    except:
-        return False
-
-
-def check_id():
-    if phone_book:
-        return max(list(map(int, phone_book))) + 1
-    return 1
-
-
-def add_contact(new: {int: dict[str, str]}):
-    contact = {int(check_id()): new}
-    phone_book.update(contact)
-
-
-def search(word: str) -> dict[int:dict[str, str]]:
-    result = {}
-    for index, contact in phone_book.items():
-        if word.lower() in ' '.join(contact.values()).lower():
-            result[index] = contact
-    return result
-
-
-def change_contact():
-    for key in phone_book:
-        p
